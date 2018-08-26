144e5ba25eacc4ac9db91bf582ef81f141b6c11b
Shengtao Li <sevenlist0110@gmail.com>
2018-08-26 15:39:58 -0700

    path sum iii

---
+++ 437_Path_Sum_3.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} sum
+ * @return {number}
+ */
+function TreeNode(val) {
+  this.val = val;
+  this.left = this.right = null;
+}
+
+var pathSum = function(root, sum) {
+  if (!root) return 0;
+  return sumUp(root, 0, sum) + pathSum(root.left, sum) + pathSum(root.right, sum);
+};
+
+function sumUp(node, pre, sum) {
+  if (!node) return 0;
+  var cur = pre + node.val;
+  return (cur === sum) + sumUp(node.left, cur, sum) + sumUp(node.right, cur, sum);
+}
