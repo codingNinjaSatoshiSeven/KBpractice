6f3dea36e8009607e38a9a93e224928b6b72c921
7seven7lst <shengtaoli0110@gmail.com>
2018-09-05 17:12:19 -0700

    [update] OPTIMIZED sum of square numbers

--- math/633_Sum_of_Square_Numbers.js
+++ math/633_Sum_of_Square_Numbers.js
@@ -3,14 +3,11 @@
  * @return {boolean}
  */
 var judgeSquareSum = function(c) {
-  var map = {};
-  for (var i = 0; i <= Math.sqrt(c); i++) {
-    var sq = i * i;
-    map[sq] = i;
-  }
-  for (var key in map) {
-    var diff = c - (+key);
-    if (diff in map) return true;
+  var a = 0, b = Math.round(Math.sqrt(c));
+  while (a <= b) {
+    if (a*a + b*b === c) return true;
+    else if  ( a*a + b*b <c) a++;
+    else b--;
   }
   return false;
 };
