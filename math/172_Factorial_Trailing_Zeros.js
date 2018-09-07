a37501eb6720bf5271f268ce05cbc7088a0c7486
7seven7lst <shengtaoli0110@gmail.com>
2018-09-07 16:50:48 -0700

    [update] REVIEW factorial trailing zeros

---
+++ math/172_Factorial_Trailing_Zeros.js
@@ -0,0 +1,17 @@
+///TODO: REVIEW
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var trailingZeroes = function(n) {
+  if (n < 0) return -1;
+  var count = 0; 
+  while ( n >=5) {
+    var rem = parseInt(n/5);
+    count += rem;
+    n = rem;
+  }
+  return count;
+};
+
+console.log(trailingZeroes(25));
