8a7860623590d967a28648a34fb2e0686cca3c3e
7seven7lst <shengtaoli0110@gmail.com>
2018-09-07 17:01:03 -0700

    [fix] longest should be max

--- string/521_Longest_Uncommon_Subsequence_1.js
+++ string/521_Longest_Uncommon_Subsequence_1.js
@@ -6,7 +6,7 @@
  */
 var findLUSlength = function(a, b) {
   if (a === b) return -1;
-  return Math.min(a.length, b.length);  
+  return Math.max(a.length, b.length);  
 };
 
 console.log(findLUSlength("aba", "cdc"));
