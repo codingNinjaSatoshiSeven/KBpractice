069550d466a33195e8c9db3ffb32d61ece011e02
7seven7lst <shengtaoli0110@gmail.com>
2018-09-11 13:34:20 -0700

    [update] REVIEW binary tree inorder traversal

---
+++ tree/94_Binary_Tree_Inorder_Traversal.js
@@ -0,0 +1,30 @@
+///TODO: REVIEW
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+function TreeNode(val) {
+  this.val = val;
+  this.left = this.right = null;
+}
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function(root) {
+  var stack = [], res = [];
+  var cur = root;
+  while (cur || stack.length >0) {
+    while (cur) {
+      stack.push(cur);
+      cur = cur.left;
+    }
+    cur = stack.pop();
+    res.push(cur.val);
+    cur = cur.right;
+  }
+  return res;
+};
