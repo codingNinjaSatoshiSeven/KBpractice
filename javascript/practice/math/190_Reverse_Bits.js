8f77c254e63c24d9d91dc342220757b6a6b5b529
7seven7lst <shengtaoli0110@gmail.com>
2018-10-29 19:27:57 -0700

    [update] REVIEW reverse bits

--- javascript/practice/math/190_Reverse_Bits.js
+++ javascript/practice/math/190_Reverse_Bits.js
@@ -1,18 +1,15 @@
+//TODO: REVIEW
 /**
  * @param {number} n - a positive integer
  * @return {number} - a positive integer
  */
 var reverseBits = function(n) {
-  var res = 0;
-  for (var i = 0; i< 32; i++) {
-    if (n & 1 === 1) {
-      res = (res << 1) + 1;
-    } else {
-      res = res << 1;
-    }
-    n = n >> 1;
+  var result = 0;
+  for (var i = 0; i < 32; i++) {
+    result |= (n >> i & 0x1) << (31 - i);
   }
-  return res;
+  return result >>> 0;
+
 };
 
 console.log(reverseBits(43261596));
