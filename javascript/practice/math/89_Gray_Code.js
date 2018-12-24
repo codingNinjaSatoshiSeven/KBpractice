2b8d2f58d28c23a2072a9d07d9aed35a9b914012
7seven7lst <shengtaoli0110@gmail.com>
2018-12-23 17:25:03 -0800

    [update] gray code

---
+++ javascript/practice/math/89_Gray_Code.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+var grayCode = function(n) {
+  var res = [];
+  for (var i = 0; i < Math.pow(2, n); i++) {
+    res.push((i>>1) ^ i);
+  }
+  return res;
+};
+console.log(grayCode(2));
