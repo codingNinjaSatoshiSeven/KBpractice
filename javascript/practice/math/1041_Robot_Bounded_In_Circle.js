ebb3fe11526b10693f828af8b6df0daf06359be2
7seven7lst <shengtaoli0110@gmail.com>
2019-05-28 16:17:16 +0800

    [update] robot bounded in circle

---
+++ javascript/practice/math/1041_Robot_Bounded_In_Circle.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} instructions
+ * @return {boolean}
+ */
+var isRobotBounded = function(instructions) {
+  let x = 0, y = 0, i = 0;
+  let d = [[0,1], [1,0], [0,-1], [-1,0]];
+  for(let j = 0; j < instructions.length; j++) {
+    if(instructions[j] === 'R') {
+      i = (i + 1) % 4;
+    } else if (instructions[j] === 'L') {
+      i = (i + 3) % 4;
+    } else {
+      x += d[i][0];
+      y += d[i][1];
+    }
+  }
+  return x === 0 && y === 0 || i > 0;
+};
